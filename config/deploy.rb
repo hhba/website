51a7c4dc57aed67c23b5a76d28627d65eb7a5435
Martin Sarsale <martin@sumavisos.com>
2011-10-22 00:10:01 -0300

    HTML5boilerplate

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "website"
+set :repository,  "git@github.com:hhba/website.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "hhba.info"                          # Your HTTP server, Apache/etc
+set :user, "www-data"
+set :deploy_to, "/var/www/website"
+set :use_sudo, false
+
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
